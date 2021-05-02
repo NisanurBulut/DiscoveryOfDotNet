b774017ef5fb7354b58dca9740578e55c61f3264
Nisanur BULUT <nbulut@sarplojistik.com>
2021-05-02 16:17:35 +0300

    created ship MVC

---
+++ Starfighter/Starfighter/Controllers/ShipsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Starfighter.Data;
+using Starfighter.Models;
+
+namespace Starfighter.Controllers
+{
+    public class ShipsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShipsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ships
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Ship.ToListAsync());
+        }
+
+        // GET: Ships/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ship = await _context.Ship
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ship == null)
+            {
+                return NotFound();
+            }
+
+            return View(ship);
+        }
+
+        // GET: Ships/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ships/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Ship ship)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ship);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ship);
+        }
+
+        // GET: Ships/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ship = await _context.Ship.FindAsync(id);
+            if (ship == null)
+            {
+                return NotFound();
+            }
+            return View(ship);
+        }
+
+        // POST: Ships/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Ship ship)
+        {
+            if (id != ship.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ship);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShipExists(ship.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ship);
+        }
+
+        // GET: Ships/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ship = await _context.Ship
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ship == null)
+            {
+                return NotFound();
+            }
+
+            return View(ship);
+        }
+
+        // POST: Ships/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ship = await _context.Ship.FindAsync(id);
+            _context.Ship.Remove(ship);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShipExists(int id)
+        {
+            return _context.Ship.Any(e => e.Id == id);
+        }
+    }
+}
