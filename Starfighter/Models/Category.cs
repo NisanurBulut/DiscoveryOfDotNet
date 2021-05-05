8723c90d153fe5c8cf5735a9f18fe9aec6adc39e
Nisanur BULUT <nbulut@sarplojistik.com>
2021-05-05 17:07:40 +0300

    created category model

---
+++ Starfighter/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Starfighter.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
