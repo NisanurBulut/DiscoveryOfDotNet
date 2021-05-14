31da4a6725ed95ccb04748950eb15a2dcccefa92
Nisanur BULUT <nbulut@sarplojistik.com>
2021-05-14 11:19:06 +0300

    implemented dbcontext

--- Starfighter/Startup.cs
+++ Starfighter/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Starfighter.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,9 @@ namespace Starfighter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<StarfighterDbContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnectionString")
+                ));
             services.AddControllersWithViews();
         }
 
